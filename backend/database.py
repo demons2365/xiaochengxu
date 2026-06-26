a5cd40481cdfba2620d423977dfc5b97d5cf9e07
Admin <admin@recycle-system.com>
2026-06-26 13:28:35 +0800

    add PostgreSQL support, remove MySQL deps

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
-# SQLite needs check_same_thread, MySQL does not
+# SQLite needs check_same_thread, other databases do not
 _connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(DATABASE_URL, connect_args=_connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
